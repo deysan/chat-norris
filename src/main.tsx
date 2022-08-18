fb9536fffe591fd85f6f623985cd08723cfe6211
deysan <ideysan@gmail.com>
2022-08-18 19:08:16 +0300

    Add persistor

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,22 @@
 import './index.css';
 import './firebase';
 
+import { persistor, store } from './store';
+
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { store } from './store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,32 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
+
+import storage from 'redux-persist/lib/storage';
 import userReducer from './slices/userSlice';
 
-export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+const reducers = combineReducers({
+  user: userReducer,
 });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+  devTools: import.meta.env.MODE !== 'production',
+});
+
+const persistor = persistStore(store);
+
+export { store, persistor };
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
