b8c15bd157256e32eab6fec54b7c0921077879b7
deysan <ideysan@gmail.com>
2022-08-23 17:53:44 +0300

    Add types

---
+++ src/types/index.ts
@@ -0,0 +1,20 @@
+export type User = {
+  id: number;
+  email: string;
+  first_name: string;
+  last_name: string;
+  avatar: string;
+};
+
+export type Profile = {
+  id: number;
+  email: string;
+  username: string;
+  photo: string;
+};
+
+export type Chat = {
+  id?: string;
+  users: string[];
+  profile: Profile;
+};
